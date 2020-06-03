b988281701c994931f8a72ede5e3a38d89de7e28
Niccolò <ssnnca@gmail.com>
2020-06-03 04:00:29 +0200

    remove for conflits

--- utils.py
+++
@@ -1,9 +0,0 @@
-import sys, os.path, shutil
-
-
-def clean_folder(folder_name):
-    try:
-        shutil.rmtree(folder_name)
-    except OSError as e:
-        pass
-    os.makedirs(folder_name, exist_ok=True)
